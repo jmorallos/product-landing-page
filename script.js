c50c0675ad43bc9100d6b3db95c097d6e84b0c45
jmorallos <joevermorallos2001@gmail.com>
2026-04-30 13:38:12 +0800

    add: overlay, closes sidenav when click/touch

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const myNav = document.getElementById("mySideNav");
+const overlay = document.getElementById("overlay");
 
 const openNav = () => {
     myNav.classList.add("is-open");
+    overlay.style.display = "block";
 }
 
 const closeNav = () => {
     myNav.classList.remove("is-open");
+    overlay.style.display = "none";
 }
